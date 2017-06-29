e5771ad63d23015cc66b0504fb09be664a5d1b7d
xuhui546 <xuhui546@hotmail.com>
2017-06-29 23:49:43 +0800

    Modify cfg_notebook.py for jupyter

--- cfg_notebook.py
+++ cfg_notebook.py
@@ -7,7 +7,6 @@ from IPython.lib import passwd
 # argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true", default=False)
-parser.add_argument("-n", "--confname", help="set ipython config folder name(default:mynbs)", action="store", default='mynbs')
 parser.add_argument("-p", "--password", help="set ipython login password(default:random)", action="store")
 parser.add_argument("-l", "--listen", help="set ipython server listen port(default:8888)", type=int, action="store", default=8888)
 args = parser.parse_args()
@@ -15,10 +14,8 @@ args = parser.parse_args()
 # set argument
 iPort = args.listen
 bVerbose = args.verbose
-sConfName = args.confname
-sHome = os.path.expanduser('~')
-sPath = sHome+'/.ipython/profile_{}/'.format(sConfName)
-sFilename = 'ipython_notebook_config.py'
+sPath = os.path.expanduser('~') + '/.jupyter/'
+sFilename = 'jupyter_notebook_config.py'
 
 if os.path.exists(sPath):
     print(sPath + ' already exists.\nexit configure.')
@@ -30,14 +27,14 @@ sSha1 = passwd(sPassword)
 
 
 # create ipython profile
-s = subprocess.Popen('ipython profile create {}'.format(sConfName), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+s = subprocess.Popen('jupyter notebook --generate-config', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 o = s.communicate()
 bVerbose and print(o[0].decode())
 bVerbose and print(o[1].decode())
 
 # create private key and certificate
 s = subprocess.Popen("""
-openssl req -x509 -nodes -days 1000 -newkey rsa:1024 -keyout {sHome}/mycert.pem -out {sHome}/mycert.pem <<EOF
+openssl req -x509 -nodes -days 1000 -newkey rsa:1024 -keyout {sPath}mykey.key -out {sPath}mycert.pem <<EOF
 
 
 
@@ -46,8 +43,8 @@ openssl req -x509 -nodes -days 1000 -newkey rsa:1024 -keyout {sHome}/mycert.pem 
 
 
 EOF
-""".format(sHome=sHome), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-bVerbose and print('create private key and certificate in {}'.format(sHome))
+""".format(sPath=sPath), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+bVerbose and print('create private key and certificate in {}'.format(sPath))
 
 # set ipython notbook file
 with open(sPath+sFilename, 'w+') as f:
@@ -56,20 +53,23 @@ with open(sPath+sFilename, 'w+') as f:
 #### Add these lines to the top of the file; no other changes necessary
 #### Obviously, you'll want to add your path to the .pem key and your password
 
-# Configuration file for ipython-notebook.
+## The full path to an SSL/TLS certificate file.
+c.NotebookApp.certfile = u'{sPath}mycert.pem'
 
-c = get_config()
-
-# Kernel config
-c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
-
-c.NotebookApp.certfile = u'{}/mycert.pem'
+## The IP address the notebook server will listen on.
 c.NotebookApp.ip = '*'
+
+## The full path to a private key file for usage with SSL/TLS.
+c.NotebookApp.keyfile = u'{sPath}mykey.key'
+
+c.NotebookApp.password = u'{sSha1}'
+
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'{}'
-# It is a good idea to put it on a known, fixed port
-c.NotebookApp.port = {}
-""".format(sHome, sSha1, iPort))
+
+## The port the notebook server will listen on.
+c.NotebookApp.port = {iPort} 
+
+""".format(sPath=sPath, sSha1=sSha1, iPort=iPort))
 bVerbose and print('{} has configured in {}'.format(sFilename, sPath))
 
 print("""
@@ -79,9 +79,4 @@ Notebook certificate path: {}
 Notebook password: {}
 Notebook port: {}
 ============================================================
-""".format(sPath, sHome, sPassword, iPort))
-
-
-
-
-
+""".format(sPath, sPath, sPassword, iPort))
